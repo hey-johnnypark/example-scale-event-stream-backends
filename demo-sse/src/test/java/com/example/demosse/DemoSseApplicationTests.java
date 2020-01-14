3c78cbc5d4d8071016f1ae4b6d268cfb2259006e
Johnny Park <johnnypark@mailbox.org>
2020-01-14 15:05:36 +0000

    Adds Kafka for testing

--- demo-sse/src/test/java/com/example/demosse/DemoSseApplicationTests.java
+++ demo-sse/src/test/java/com/example/demosse/DemoSseApplicationTests.java
@@ -1,13 +1,21 @@
 package com.example.demosse;
 
+import org.junit.ClassRule;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.kafka.test.rule.EmbeddedKafkaRule;
+
 
 @SpringBootTest
 class DemoSseApplicationTests {
 
+	@ClassRule
+	public static EmbeddedKafkaRule embeddedKafka = new EmbeddedKafkaRule(1, true, "sse");
+
+
 	@Test
-	void contextLoads() {
+	void contextLoads() throws InterruptedException {
+		Thread.sleep(10000);
 	}
 
 }
